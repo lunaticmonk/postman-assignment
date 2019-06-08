09017af9abd4b641b48df2d0fadde189133c1408
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-06-08 15:29:54 +0530

    :wrench: Add the database connection on server ignition

--- configs/app.js
+++ configs/app.js
@@ -3,11 +3,18 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { APP_PORT, ALLOWED_DOMAINS } = process.env;
+const {
+  APP_PORT,
+  ALLOWED_DOMAINS,
+  DATABASE_USER,
+  DATABASE_PASSWORD
+} = process.env;
 
 const configs = {
   APP_PORT,
-  ALLOWED_DOMAINS
+  ALLOWED_DOMAINS,
+  DATABASE_USER,
+  DATABASE_PASSWORD
 };
 
 module.exports = configs;
--- src/server.js
+++ src/server.js
@@ -3,17 +3,23 @@
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const userRouter = require("./routers/user");
 const tweetRouter = require("./routers/tweet");
 
-const { ALLOWED_DOMAINS } = require("../configs/app");
+const {
+  ALLOWED_DOMAINS,
+  DATABASE_USER,
+  DATABASE_PASSWORD
+} = require("../configs/app");
 
 class Server {
   constructor() {
     this.app = express();
     this.config();
     this.routes();
+    this.initializeDb();
   }
 
   config() {
@@ -42,6 +48,15 @@ class Server {
         return res.status(err.status).send(response);
       }
     });
+  }
+
+  initializeDb() {
+    mongoose.connect(
+      `mongodb://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:27017/postman`,
+      {
+        useNewUrlParser: true
+      }
+    );
   }
 }
 
